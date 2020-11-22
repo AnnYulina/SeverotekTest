3d4867d20fbbc58389ea19be4568de0762e710d4
likx <likx@inbox.ru>
2020-11-22 14:35:24 +0300

    sd

--- src/test/java/igorakintev/constants/Const.java
+++ src/test/java/igorakintev/constants/Const.java
@@ -4,7 +4,7 @@ public class Const {
     public static final String MAIN_PAGE = "https://igorakintev.ru/admin/";
     public static final String BLOG_PAGE = "https://igorakintev.ru/blog/";
     public static final String ADMIN_PAGE = "https://igorakintev.ru/admin/blog/";
-    public static final String CHROME_DRIVER = "C:\\WebDrivers\\chromedriver.exe";
-    public static final String FIREFOX_DRIVER = "C:\\WebDrivers\\geckodriver.exe";
+    public static final String CHROME_DRIVER = "./src/main/resources/chromedriver.exe";
+    public static final String FIREFOX_DRIVER = "./src/main/resources/geckodriver.exe";
 
 }
